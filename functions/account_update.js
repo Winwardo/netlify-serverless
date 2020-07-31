ac7bfd291c784405c8ff5916cf747a359bf72dc8
Topher Winward <winwardo@gmail.com>
2020-07-31 12:13:59 +0100

    Create account_update.js

---
+++ functions/account_update.js
@@ -0,0 +1,18 @@
+exports.handler = async function handler(event, context) {
+  console.log("=====");
+  console.log(event);
+  console.log("--");
+  console.log(event);
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ hello: "world" })
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    }
+  }
+}
